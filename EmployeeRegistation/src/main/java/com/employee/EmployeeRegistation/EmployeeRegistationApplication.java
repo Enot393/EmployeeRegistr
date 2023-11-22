110b411c690047649908c3ec61820194429a174c
Enot272 <jecky432@gmail.com>
2023-11-22 04:50:38 +0300

    Initial commit

---
+++ EmployeeRegistation/src/main/java/com/employee/EmployeeRegistation/EmployeeRegistationApplication.java
@@ -0,0 +1,13 @@
+package com.employee.EmployeeRegistation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegistationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRegistationApplication.class, args);
+	}
+
+}
