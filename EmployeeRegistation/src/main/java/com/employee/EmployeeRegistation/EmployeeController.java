ddc7724c8d11f7ec729a2395796db0624db9d0bd
Enot272 <jecky432@gmail.com>
2023-11-22 07:37:18 +0300

    add logic

--- EmployeeRegistation/src/main/java/com/employee/EmployeeRegistation/EmployeeController.java
+++ EmployeeRegistation/src/main/java/com/employee/EmployeeRegistation/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.employee.EmployeeRegistation;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,9 +11,26 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
-
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
+    }
 
+    @GetMapping(path = "add")
+    public Employee addEmployee(@RequestParam(value = "firstName", required = false) String firstName,
+                                @RequestParam(value = "lastName", required = false) String lastName) {
+        return employeeService.addEmployee(firstName, lastName);
+    }
+
+    @GetMapping(path = "remove")
+    public Employee removeEmployee(@RequestParam(value = "firstName", required = false) String firstName,
+                                   @RequestParam(value = "lastName", required = false) String lastName) {
+        return employeeService.removeEmployee(firstName, lastName);
+    }
+
+
+    @GetMapping(path = "search")
+    public Employee searchEmployee(@RequestParam(value = "firstName", required = false) String firstName,
+                                   @RequestParam(value = "lastName", required = false) String lastName) {
+        return employeeService.searchEmployee(firstName, lastName);
     }
 }
--- EmployeeRegistation/src/main/java/com/employee/EmployeeRegistation/EmployeeServiceImpl.java
+++ EmployeeRegistation/src/main/java/com/employee/EmployeeRegistation/EmployeeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.employee.EmployeeRegistation;
 
+import com.employee.EmployeeRegistation.exceptions.*;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,21 +14,38 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee addEmployee(String firstName, String lastName) {
-        // EmployeeStorageIsFullException
-        // EmployeeAlreadyAddedException
-        return null;
+        if (employeeBook.size() >= employeeBookLimit) {
+            throw new EmployeeStorageIsFullException("ArrayIsFull");
+        }
+        for (Employee employee : employeeBook) {
+            if (employee.getFirstName().equals(firstName) && employee.getLastName().equals(lastName)) {
+                throw new EmployeeAlreadyAddedException("EmployeeAlreadyAdded");
+            }
+        }
+        Employee employee = new Employee(firstName, lastName);
+        employeeBook.add(employee);
+        return employee;
     }
 
 
     @Override
     public Employee removeEmployee(String firstName, String lastName) {
-        // EmployeeNotFoundException
-        return null;
+        for (Employee employee : employeeBook) {
+            if (employee.getFirstName().equals(firstName) && employee.getLastName().equals(lastName)) {
+                employeeBook.remove(employee);
+                return employee;
+            }
+        }
+        throw new EmployeeNotFoundException("EmployeeNotFound");
     }
 
     @Override
     public Employee searchEmployee(String firstName, String lastName) {
-        // EmployeeNotFoundException
-        return null;
+        for (Employee employee : employeeBook) {
+            if (employee.getFirstName().equals(firstName) && employee.getLastName().equals(lastName)) {
+                return employee;
+            }
+        }
+        throw new EmployeeNotFoundException("EmployeeNotFound");
     }
 }
