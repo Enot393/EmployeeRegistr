a078195201b7e94f1746285eb47b675eedf4742d
Enot272 <jecky432@gmail.com>
2023-11-23 11:39:24 +0300

    ready?

--- EmployeeRegistation/src/main/java/com/employee/EmployeeRegistation/EmployeeController.java
+++ EmployeeRegistation/src/main/java/com/employee/EmployeeRegistation/EmployeeController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedList;
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/employee")
 public class EmployeeController {
