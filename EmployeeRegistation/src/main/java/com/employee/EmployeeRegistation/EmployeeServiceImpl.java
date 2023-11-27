b2cd2950e1efa792067803e3aeac16cb414cd029
Enot272 <jecky432@gmail.com>
2023-11-27 15:08:34 +0300

    moving to HashMap

--- EmployeeRegistation/src/main/java/com/employee/EmployeeRegistation/EmployeeServiceImpl.java
+++ EmployeeRegistation/src/main/java/com/employee/EmployeeRegistation/EmployeeServiceImpl.java
@@ -3,51 +3,47 @@ package com.employee.EmployeeRegistation;
 import com.employee.EmployeeRegistation.exceptions.*;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private List<Employee> employeeBook;
+    private Map<String, Employee> employeeBook;
     private final int employeeBookLimit = 10;
 
-    public EmployeeServiceImpl(List<Employee> employeeBook) {
+    public EmployeeServiceImpl(Map<String, Employee> employeeBook) {
         this.employeeBook = employeeBook;
     }
+
     @Override
     public Employee addEmployee(String firstName, String lastName) {
         if (employeeBook.size() >= employeeBookLimit) {
-            throw new EmployeeStorageIsFullException("ArrayIsFull");
+            throw new EmployeeStorageIsFullException("StorageIsFull");
         }
-        for (Employee employee : employeeBook) {
-            if (employee.getFirstName().equals(firstName) && employee.getLastName().equals(lastName)) {
-                throw new EmployeeAlreadyAddedException("EmployeeAlreadyAdded");
-            }
+        if (employeeBook.containsKey(firstName + lastName)) {
+            throw new EmployeeAlreadyAddedException("EmployeeAlreadyAdded");
+        } else {
+            Employee employee = new Employee(firstName, lastName);
+            employeeBook.put(firstName + lastName, employee);
+            return employee;
         }
-        Employee employee = new Employee(firstName, lastName);
-        employeeBook.add(employee);
-        return employee;
     }
-
 
     @Override
     public Employee removeEmployee(String firstName, String lastName) {
-        for (Employee employee : employeeBook) {
-            if (employee.getFirstName().equals(firstName) && employee.getLastName().equals(lastName)) {
-                employeeBook.remove(employee);
-                return employee;
-            }
+
+        if (employeeBook.containsKey(firstName + lastName)) {
+            Employee employeeForReturn = new Employee(firstName, lastName);
+            employeeBook.remove(firstName + lastName);
+            return employeeForReturn;
         }
         throw new EmployeeNotFoundException("EmployeeNotFound");
     }
 
     @Override
     public Employee searchEmployee(String firstName, String lastName) {
-        for (Employee employee : employeeBook) {
-            if (employee.getFirstName().equals(firstName) && employee.getLastName().equals(lastName)) {
-                return employee;
-            }
+        if (employeeBook.containsKey(firstName + lastName)) {
+            return employeeBook.get(firstName + lastName);
         }
         throw new EmployeeNotFoundException("EmployeeNotFound");
     }
